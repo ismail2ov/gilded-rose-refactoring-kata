6786668fd8fdd1d28e05290c156cb5e7614d0d88
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-12-21 06:01:24 +0100

    Use CombinationApprovals for 100% code coverage

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,32 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void update_quality() {
+
+        String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] sellIn = {-1, 0, 11};
+        Integer[] qualities = {0, 1, 49, 50};
+
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                names,
+                sellIn,
+                qualities);
     }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        return app.items[0].toString();
+    }
+
 
 }
